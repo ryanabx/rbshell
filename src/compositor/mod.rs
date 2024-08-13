6a6fc56da086081456f9e60d9916a94894bd4c3a
Ryan Brue <ryanbrue@gmail.com>
2024-08-13 16:43:52 -0500

    feat: set RYANABX_SHELL_DESKTOP to set the desktop session, defaults to XDG_CURRENT_DESKTOP

--- src/compositor/mod.rs
+++ src/compositor/mod.rs
@@ -13,7 +13,12 @@ pub enum WaylandMessage {
 }
 
 pub(crate) fn wayland_subscription() -> iced::Subscription<WaylandMessage> {
-    match env::var("XDG_CURRENT_DESKTOP").as_deref() {
+    // set the environment variable RYANABX_SHELL_DESKTOP to set which desktop session should be inferred
+    let current_compositor = match env::var("RYANABX_SHELL_DESKTOP") {
+        Ok(val) => Ok(val),
+        _ => env::var("XDG_CURRENT_DESKTOP"), // fall back on XDG_CURRENT_DESKTOP if not set
+    };
+    match current_compositor.as_deref() {
         Ok("COSMIC") => cosmic_comp::wayland_subscription().map(WaylandMessage::CosmicComp),
         _ => panic!("Unsupported desktop"),
     }
